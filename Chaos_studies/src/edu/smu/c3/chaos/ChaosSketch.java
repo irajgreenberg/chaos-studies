2cb83c5c9404680090c7c33088a5bd449402e2d8
Yong Bakos <ybakos@humanoriented.com>
2015-10-31 12:54:34 -0500

    Add chaotic overrides for remaining background methods, strokeWeight, and point.

--- Chaos_studies/src/edu/smu/c3/chaos/ChaosSketch.java
+++ Chaos_studies/src/edu/smu/c3/chaos/ChaosSketch.java
@@ -11,22 +11,25 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ChaosSketch extends PApplet {
 	
+	// TODO Replace with number generator. Issue #1.
 	static final int DIMENSION_FUCK_FACTOR = 200;
-	static final int COLOR_FUCK_FACTOR = 2;
+	static final int COLOR_FUCK_FACTOR = 10;
+	static final int STROKE_FUCK_FACTOR = 4;
+	static final int COORDINATE_FUCK_FACTOR = 50;
 	
 	private ThreadLocalRandom r = ThreadLocalRandom.current();
 	
 	@Override
 	public void size(int width, int height) {
 		super.size(width + r.nextInt(-DIMENSION_FUCK_FACTOR, DIMENSION_FUCK_FACTOR),
-				height + r.nextInt(-DIMENSION_FUCK_FACTOR, DIMENSION_FUCK_FACTOR));
+				   height + r.nextInt(-DIMENSION_FUCK_FACTOR, DIMENSION_FUCK_FACTOR));
 	}
 	
 	@Override
 	public void size(int width, int height, String renderer) {
 		super.size(width + r.nextInt(-DIMENSION_FUCK_FACTOR, DIMENSION_FUCK_FACTOR),
-				height + r.nextInt(-DIMENSION_FUCK_FACTOR, DIMENSION_FUCK_FACTOR),
-				renderer);
+				   height + r.nextInt(-DIMENSION_FUCK_FACTOR, DIMENSION_FUCK_FACTOR),
+				   renderer);
 	}
 	
 	/* Drawing methods */
@@ -38,27 +41,58 @@ public class ChaosSketch extends PApplet {
 	
 	@Override
 	public void background(int rgb, float alpha) {
-		super.background(rgb, alpha);
+		super.background(rgb + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR),
+						 alpha + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR));
 	}
-
+	
+	@Override
     public void background(float gray) {
-    	super.background(gray);
+    	super.background(gray + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR));
 	}
 
+	@Override
     public void background(float gray, float alpha) {
-		super.background(gray, alpha);
+		super.background(gray + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR),
+						 alpha + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR));
 	}
 
+	@Override
     public void background(float v1, float v2, float v3) {
-    	super.background(v1, v2, v3);
+    	super.background(v1 + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR),
+    					 v2 + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR),
+    					 v3 + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR));
 	}
 
+	@Override
     public void background(float v1, float v2, float v3, float alpha) {
-		super.background(v1, v2, v3, alpha);
+		super.background(v1 + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR),
+						 v2 + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR),
+						 v3 + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR),
+						 alpha + r.nextInt(-COLOR_FUCK_FACTOR, COLOR_FUCK_FACTOR));
 	}
     
+	@Override
     public void background(PImage image) {
+    	// TODO Glitch out the PImage. Issue #2.
     	super.background(image);
+	}
+	
+	@Override
+	public void strokeWeight(float weight) {
+		super.strokeWeight(weight + r.nextInt(-STROKE_FUCK_FACTOR, STROKE_FUCK_FACTOR));
+	}
+	
+	@Override
+	public void point(float x, float y) {
+		super.point(x + r.nextInt(-COORDINATE_FUCK_FACTOR, COORDINATE_FUCK_FACTOR),
+					y + r.nextInt(-COORDINATE_FUCK_FACTOR, COORDINATE_FUCK_FACTOR));
+	}
+
+	@Override
+	public void point(float x, float y, float z) {
+		super.point(x + r.nextInt(-COORDINATE_FUCK_FACTOR, COORDINATE_FUCK_FACTOR),
+					y + r.nextInt(-COORDINATE_FUCK_FACTOR, COORDINATE_FUCK_FACTOR),
+					z + + r.nextInt(-COORDINATE_FUCK_FACTOR, COORDINATE_FUCK_FACTOR));
 	}
 	
 }
